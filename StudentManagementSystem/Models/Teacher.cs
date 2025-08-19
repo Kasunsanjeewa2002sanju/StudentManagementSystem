b859f0910a07b845266a486856161dcab61f29d2
Kasunsanjeewa2002sanju <kasunsanjeewa2002@gmail.com>
2025-08-19 22:32:22 +0530

    V1

---
+++ StudentManagementSystem/Models/Teacher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagementSystem.Models
+{
+    public class Teacher
+    {
+        [Key]
+        public int TeacherId { get; set; }
+
+        [Required, StringLength(100)]
+        public required string Name { get; set; }
+
+        [Required, StringLength(100)]
+        public required string Subject { get; set; }
+
+        [EmailAddress]
+        public required string Email { get; set; }
+
+        [Phone]
+        public string? Phone { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime HireDate { get; set; }
+    }
+}
